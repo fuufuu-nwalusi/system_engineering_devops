5fc59ff82bfe95c3feab74fda0ff3b2a987d74f0
Epique Buff <oliviaujunwa851@gmail.com>
2023-09-11 16:45:44 +0100

    Updated

--- learning_C/tutorials.c
+++ learning_C/tutorials.c
@@ -2,7 +2,38 @@
 
 int main(void)
 {
-	printf("Checking out C\n");
+	int score = 100;
+	printf("I made %i%% in Software Engineering\n", score); 
+
+
+	//printf("Checking out C\n");
+	
+/*	puts("Yes, this is what puts do. It automatically adds a new line");
+	
+	putchar('L');
+	putchar('e');
+	putchar('t');
+	putchar('\'');
+	putchar('s');
+	putchar(' ');
+	putchar('s');
+	putchar('e');
+	putchar('e');
+	putchar(' ');
+	putchar('w');
+	putchar('h');
+	putchar('a');
+	putchar('t');
+	putchar(' ');
+	putchar('h');
+	putchar('a');
+	putchar('p');
+	putchar('p');
+	putchar('e');
+	putchar('n');
+	putchar('s');
+	putchar('\n');
+*/
 	return 0;
 }
 
