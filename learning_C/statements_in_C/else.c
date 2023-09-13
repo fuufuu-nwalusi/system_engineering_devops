3db431c952b55e96cc116eaa9f6b9dd92814cb1d
Epique Buff <oliviaujunwa851@gmail.com>
2023-09-13 22:14:07 +0100

    Else statement

---
+++ learning_C/statements_in_C/else.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	int boyAge = 29;
+	int girlAge = 15;
+
+	if (boyAge >= girlAge)
+	{
+		printf("Good to go");
+	}
+	else
+	{
+		printf("That's not okay");
+	}
+	
+	return 0;
+}
