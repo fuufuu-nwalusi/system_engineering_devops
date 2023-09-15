c203cb776daec6e2b02f3cfdcda41cb06ab4696e
Epique Buff <oliviaujunwa851@gmail.com>
2023-09-15 22:28:31 +0100

    Print alphabets in low case

---
+++ learning_C/001-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+ * main - prints alphabets in lower case
+ *
+ * Return: 0 on sucess
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while(c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	return 0;
+}
