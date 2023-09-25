d32aec6f4f87913e67c4fe6cd0107c3e038add0b
Epique Buff <oliviaujunwa851@gmail.com>
2023-09-25 16:45:39 +0100

    Learning Functions

---
+++ learning_C/0x02-functions/alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+//void printAlphabet();
+
+int main(void)
+{
+	printf("Alphabets in lower case:\n");
+
+//	printAlphabet();
+
+	return 0;
+}
+/*
+void printAlphabet()
+{
+	char alpha;
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		printf("%c ", alpha);
+	}
+	printf("\n");
+}
+*/
