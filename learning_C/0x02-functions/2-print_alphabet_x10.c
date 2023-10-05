393515ba3f5104a052885ab1e0e181dfcd18e19f
Fuufuu <ifunanyanwalusiuka@gmail.com>
2023-10-05 16:44:24 -0700

    Prime Number

---
+++ learning_C/0x02-functions/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * print_alphabet_x10: Make alphabet x10 times
+ *
+ * Return: Always 0
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	
+}
---
+++ learning_C/0x02-functions/primeNum.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+ * checkPrimeNumber: function used to check if a number is a primenumber or not
+ *
+ * return: Always 0 on success
+ */
+
+void checkPrimeNumber();
+
+void checkPrimeNumber()
+{
+	int n, i, flag = 0;
+
+	printf("Enter a positive integer: ");
+	scanf("%d", &n);
+
+}
